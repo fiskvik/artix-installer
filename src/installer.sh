550f36221b95f8a2f4ed109639a23f1dad8ddacd
Torstein <Torstein Fiskvik torstein.fiskvik@gmail.com>
2024-11-29 12:25:30 +0100

    Added openssh.

--- src/installer.sh
+++ src/installer.sh
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with artix-installer. If not, see <https://www.gnu.org/licenses/>.
 
-pkgs="base base-devel $MY_INIT elogind-$MY_INIT efibootmgr grub dhcpcd wpa_supplicant connman-$MY_INIT"
+pkgs="base base-devel $MY_INIT elogind-$MY_INIT efibootmgr grub dhcpcd wpa_supplicant connman-$MY_INIT openssh openssh-$MY_INIT"
 [ "$MY_FS" = "btrfs" ] && pkgs="$pkgs btrfs-progs"
 [ "$ENCRYPTED" = "y" ] && pkgs="$pkgs cryptsetup cryptsetup-$MY_INIT"
 
