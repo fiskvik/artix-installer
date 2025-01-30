21a8602ad74eebfc463175230fcfaa34f1fdc7f3
fiskvik <1080388+fiskvik@users.noreply.github.com>
2025-01-30 16:40:51 +0100

    Update telegraf-install.sh

--- src/telegraf-install.sh
+++ src/telegraf-install.sh
@@ -5,4 +5,5 @@ sudo -Eu nobody git clone https://aur.archlinux.org/telegraf.git
 cd /tmp/telegraf
 sudo -Eu nobody makepkg -f
 pacman -U --noconfirm *.zst
+useradd --system --home-dir /var/lib/telegraf --shell=/usr/bin/nologin telegraf
 export HOME=/root
